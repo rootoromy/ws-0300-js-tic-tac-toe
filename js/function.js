f4f3a5858163aa2f384bb2b92f2d9cc295c9d59c
bu-yasutake <h.yasutake@brewus.co.jp>
2024-10-18 13:16:59 +0900

    フィードバック対応

--- js/function.js
+++ js/function.js
@@ -1,9 +1,9 @@
 'use strict'
 //セルのID
-const tableIds = [id_1, id_2, id_3, id_4, id_5, id_6, id_7, id_8, id_9];
+const tableElements = [id_1, id_2, id_3, id_4, id_5, id_6, id_7, id_8, id_9];
 
 //勝ちパターン
-const winResult = [
+const winPattern = [
 [id_1, id_2, id_3],[id_4, id_5, id_6],[id_7, id_8, id_9],
 [id_1, id_4, id_7],[id_2, id_5, id_8],[id_3, id_6, id_9],
 [id_1, id_5, id_9],[id_3, id_5, id_7]];
@@ -12,40 +12,43 @@ let clickedCount = 0;
 
 //勝ち判定の関数
 function winJudgement(){
-  for(let j = 0; j<winResult.length; j++){
+  for(let j = 0; j<winPattern.length; j++){
 
-    if(winResult[j][0].innerHTML==="◯"&&winResult[j][1].innerHTML==="◯"&&winResult[j][2].innerHTML==="◯"){
+    if(winPattern[j][0].innerHTML==="◯"&&winPattern[j][1].innerHTML==="◯"&&winPattern[j][2].innerHTML==="◯"){
       document.getElementById('message').textContent = "勝者は「◯」です！！！";
       for(let k = 0; k<3; k++){
-        document.getElementById(winResult[j][k].id).style.backgroundColor = 'yellow';
+        winPattern[j][k].classList.add('winCircle');
       }
-    }else if(winResult[j][0].innerHTML==="×"&&winResult[j][1].innerHTML==="×"&&winResult[j][2].innerHTML==="×"){
+    }else if(winPattern[j][0].innerHTML==="×"&&winPattern[j][1].innerHTML==="×"&&winPattern[j][2].innerHTML==="×"){
       document.getElementById('message').textContent = "勝者は「×」です！！！";
       for(let k = 0; k<3; k++){
-        document.getElementById(winResult[j][k].id).style.backgroundColor = 'Skyblue';
+        winPattern[j][k].classList.add('winCross');
       }
     }
   }
 }
 
-for(let i = 0; i < tableIds.length; i++){
+for(let i = 0; i < tableElements.length; i++){
   //クリックしたら交互に◯×を出すようにする関数
-  function clickAndChange() {
+  function handleClick() {
     //◯×のターンは、◯が先行
-    const circle = document.getElementById("circle");
-    circle.classList.add('active');
-    const cross = document.getElementById("cross");
-    cross.classList.remove('active');
+    //const circle = document.getElementById("circle");
+    //circle.classList.add('active');
+    //const cross = document.getElementById("cross");
+    //cross.classList.remove('active');
 
-    tableIds[i].addEventListener("click", (e)=>{
+    tableElements[i].addEventListener("click", (e)=>{
       //セルが空のときにのみ◯×がつく。すでに入ってる時はアラート
-      if(!tableIds[i].innerHTML){
+      if(tableElements[i].innerHTML){
+        alert("1回入力したら変更できません");
+        return;
+      }
         clickedCount++;
 
 
         if(clickedCount%2 === 1){
           //◯を入れる
-          tableIds[i].innerHTML = "◯";
+          tableElements[i].innerHTML = "◯";
           //◯×のターンを切り替える
           const cross = document.getElementById("cross");
           cross.classList.add('active');
@@ -53,16 +56,14 @@ for(let i = 0; i < tableIds.length; i++){
           circle.classList.remove('active');
         } else {
           //×を入れる
-          tableIds[i].innerHTML = "×";
+          tableElements[i].innerHTML = "×";
           //◯×のターンを切り替える
           const circle = document.getElementById("circle");
           circle.classList.add('active');
           const cross = document.getElementById("cross");
           cross.classList.remove('active');
         }
-      }else{
-        alert("1回入力したら変更できません");
-      }
+
       //勝ち判定の関数を実行
       winJudgement();
     });
@@ -71,13 +72,13 @@ for(let i = 0; i < tableIds.length; i++){
   function reset(){
     button.addEventListener("click", ()=>{
       console.log("押された");
-      tableIds[i].innerHTML = "";
-      tableIds[i].style.backgroundColor = 'transparent';
+      tableElements[i].innerHTML = "";
+      tableElements[i].style.backgroundColor = 'transparent';
       document.getElementById("message").textContent = "starting...";
     });
   }
   //上記関数を実行する
-  clickAndChange();
+  handleClick();
   reset();
 }
 
