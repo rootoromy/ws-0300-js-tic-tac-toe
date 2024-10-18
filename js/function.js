bee77d579190263f36a6bd94b318ac56af3f13da
bu-yasutake <h.yasutake@brewus.co.jp>
2024-10-18 19:02:31 +0900

    フィードバックに対応。引き分け判定と、決着がついた後にセルをクリックしても反応しないように実装。

--- js/function.js
+++ js/function.js
@@ -8,33 +8,51 @@ const winPattern = [
 [id_1, id_4, id_7],[id_2, id_5, id_8],[id_3, id_6, id_9],
 [id_1, id_5, id_9],[id_3, id_5, id_7]];
 
+const table__td = document.getElementsByClassName("table__td");
+const tableTdArray = Array.prototype.slice.call(table__td);//配列に変換
+
 let clickedCount = 0;
+let circleCount = 0;
+let crossCount = 0;
+let isGameOver = false;
 
 //勝ち判定の関数
 function winJudgement(){
-  const table__td = document.getElementsByClassName('table__td');
-console.log(table__td);
+
   for(let j = 0; j<winPattern.length; j++){
 
     if(winPattern[j][0].innerHTML==="◯"&&winPattern[j][1].innerHTML==="◯"&&winPattern[j][2].innerHTML==="◯"){
       document.getElementById('message').textContent = "勝者は「◯」です！！！";
       for(let k = 0; k<3; k++){
+        winPattern[j][k].classList.remove('reset');
+        winPattern[j][k].classList.remove('winCross');
         winPattern[j][k].classList.add('winCircle');
       }
+      isGameOver = true;
+      return '◯';
     }else if(winPattern[j][0].innerHTML==="×"&&winPattern[j][1].innerHTML==="×"&&winPattern[j][2].innerHTML==="×"){
       document.getElementById('message').textContent = "勝者は「×」です！！！";
       for(let k = 0; k<3; k++){
+        winPattern[j][k].classList.remove('reset');
+        winPattern[j][k].classList.remove('winCircle');
         winPattern[j][k].classList.add('winCross');
       }
-    }else if(((table__td.innerHTML==="x")>=5)||((table__td.innerHTML==="◯")>=5)){
-      document.getElementById('message').textContent = "引き分けです！！！";
-    }
+      isGameOver = true;
+      return '×';
+    } 
+  }
+  //引き分け判定
+  if((!isGameOver&&circleCount>=5) || (!isGameOver&&crossCount>=5)){
+    document.getElementById('message').textContent = "引き分けです！！！";
+    isGameOver = true;
+    return 'Draw';
   }
 }
 
 for(let i = 0; i < tableElements.length; i++){
   //クリックしたら交互に◯×を出すようにする関数
   function handleClick() {
+    
     //◯×のターンは、◯が先行
     //const circle = document.getElementById("circle");
     //circle.classList.add('active');
@@ -42,6 +60,10 @@ for(let i = 0; i < tableElements.length; i++){
     //cross.classList.remove('active');
 
     tableElements[i].addEventListener("click", (e)=>{
+      //ゲームの決着がついている時はリターン
+      if(isGameOver){
+        return;
+      }
       //セルが空のときにのみ◯×がつく。すでに入ってる時はアラート
       if(tableElements[i].innerHTML){
         alert("1回入力したら変更できません");
@@ -54,6 +76,7 @@ for(let i = 0; i < tableElements.length; i++){
         if(clickedCount%2 === 1){
           //◯を入れる
           tableElements[i].innerHTML = "◯";
+          circleCount++;
           //◯×のターンを切り替える
           const cross = document.getElementById("cross");
           cross.classList.add('active');
@@ -62,6 +85,7 @@ for(let i = 0; i < tableElements.length; i++){
         } else {
           //×を入れる
           tableElements[i].innerHTML = "×";
+          crossCount++;
           //◯×のターンを切り替える
           const circle = document.getElementById("circle");
           circle.classList.add('active');
@@ -77,9 +101,12 @@ for(let i = 0; i < tableElements.length; i++){
   //クリックしたらリセットする関数
   function reset(){
     button.addEventListener("click", ()=>{
-      console.log("押された");
+      isGameOver = false;
+      clickedCount = 0;
+      circleCount = 0;
+      crossCount = 0;
       tableElements[i].innerHTML = "";
-      tableElements[i].style.backgroundColor = 'transparent';
+      tableElements[i].classList.add('reset');
       document.getElementById("message").textContent = "starting...";
     });
   }
@@ -87,6 +114,7 @@ for(let i = 0; i < tableElements.length; i++){
   handleClick();
   reset();
 }
+
 
 
 
